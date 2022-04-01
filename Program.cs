f10a2dbed7bb440e28cb30cd6e395abc6c3af1ef
yujj@neusoft.com <yujj@neusoft.com>
2022-04-01 10:57:14 +0800

    When err code is 0, break

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
-﻿using  System.Diagnostics;
+using  System.Diagnostics;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-while (true){
+int? ExitCode = -1;
+while (true) {
    executeCommand();
+   if (ExitCode == 0) break;
    Console.WriteLine("Finish one loop");
+   //already exist, it will be replaced with node
    Console.WriteLine();
    await Task.Delay(5000);
 }
 
-static void executeCommand() {
+//while (line != null && line.Contains("already exist, it will be replaced with node"))
+void executeCommand() {
    ProcessStartInfo processStartInfo = 
        new ProcessStartInfo("bash", "/home/burning/release/0330/build/prebuilts_download.sh");
    processStartInfo.UseShellExecute   = true;
-    processStartInfo.CreateNoWindow = false;
-    Process? process = 
-      Process.Start(processStartInfo);
+   processStartInfo.CreateNoWindow = true;
+   //zprocessStartInfo.RedirectStandardOutput = false;
+   Process? process = Process.Start(processStartInfo);
    // Notice here. Otherwise there is an error.
    process?.WaitForExit();
-   var exitCode = process?.ExitCode;
+   ExitCode = process?.ExitCode;
+   Console.WriteLine("Err code:" + ExitCode);
    process?.Close();
  }
+
 
