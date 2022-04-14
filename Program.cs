9636cccadc78be76634873b19ce6effa3d061908
小波理论 <2250911301@qq.com>
2022-04-14 11:17:56 +0800

    Get sh path by input

--- Program.cs
+++ Program.cs
@@ -1,42 +1,57 @@
-    using  System.Diagnostics;
-using ThirdParty.Json.LitJson;
+using  System.Diagnostics;
 using System.IO;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 int? ExitCode = -1;
-string ShPath;
-GetShPath();
+Console.Write("Please input prebuilts_download.sh path: ");
+string? ShPath = Console.ReadLine()?.Trim();
 //dotnet-sdk-6.0
 while (true) {
    ExecuteCommand();
    if (ExitCode == 0) break;
    Console.WriteLine("Finish one loop");
-   //already exist, it will be replaced with node
    Console.WriteLine();
    await Task.Delay(5000);
+
 }
 
-void GetShPath()
-{
-   var folder = Environment.SpecialFolder.LocalApplicationData;
-   var path = Environment.GetFolderPath(folder);
-   var config = System.IO.Path.Join(path, "config.txt");
-   ShPath =  File.ReadAllText(config).Trim();
-   Console.WriteLine(ShPath);
-}
+
 
 //while (line != null && line.Contains("already exist, it will be replaced with node"))
+
 void ExecuteCommand() {
+
+   if (ShPath == null){
+
+      Console.WriteLine("PLease input valid prebuilts_download.sh path");
+
+      return;
+
+   }
+
    ProcessStartInfo processStartInfo = 
+
        new ProcessStartInfo("bash", ShPath);
+
    processStartInfo.UseShellExecute   = true;
+
    processStartInfo.CreateNoWindow = true;
+
    //zprocessStartInfo.RedirectStandardOutput = false;
+
    Process? process = Process.Start(processStartInfo);
+
    // Notice here. Otherwise there is an error.
+
    process?.WaitForExit();
+
    ExitCode = process?.ExitCode;
+
    Console.WriteLine("Err code:" + ExitCode);
+
    process?.Close();
+
  }
+
